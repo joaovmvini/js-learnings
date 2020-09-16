f7629905f87a1197258b550c915345eee888b05f
joaovmvini <joaopfvasconcelosx@outlook.com>
2020-09-16 17:10:55 -0300

    closures: finished

--- closures.js
+++ closures.js
@@ -59,3 +59,56 @@
    *  e em sua execução, essa closure vai ocorrer.
    */
 }
+
+/* Example 6: Closures + Loops */
+
+{
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function timer() {
+      console.log(i);
+    }, i * 1000);
+  }
+  /* Bug: será impresso o número 6 em todas as iterações, por quê?
+   * isso acontece porquê a função callback timer está sendo chamada
+   * apenas quando esse loop já terminou, ou seja, quando i é 6.
+   */
+}
+
+// Solução: mais closures!
+
+{
+  for (var i = 1; i <= 5; i++) {
+    (function (i) {
+      setTimeout(function timer() {
+        console.log(i);
+      }, i * 1000);
+    })(i);
+  }
+
+  /* Adicionando esse bloco IIFE nós temos uma função imediatamente
+   * invocada, no escopo léxico do for. Portanto, o i passado como
+   * argumento agora guarda uma referência direta para cada iteração
+   * do for loop
+   */
+}
+
+// outra solução
+
+{
+  for (var i = 1; i <= 5; i++) {
+    let j = i;
+    setTimeout(function timer() {
+      console.log(j);
+    }, j * 1000);
+  }
+}
+
+// ou
+
+{
+  for (let i = 1; i <= 5; i++) {
+    setTimeout(function timer() {
+      console.log(i);
+    }, i * 1000);
+  }
+}
